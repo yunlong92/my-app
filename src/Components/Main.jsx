9c650a4cc9fcd292cb685e0cd50b0504cfda78a7
Yun Long <yun_long_@hotmail.com>
2022-10-03 23:54:36 +0200

    format document

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,58 +5,51 @@ import Pokeinfo from "./Pokeinfo";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
-const Main=()=>{
-    const [pokeData,setPokeData]=useState([]);
-    const [loading,setLoading]=useState(true);
-    const url= "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+const Main = () => {
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+  const [pokeDex, setPokeDex] = useState();
 
-    const [pokeDex,setPokeDex]=useState();
+  const pokeFun = async () => {
+    setLoading(true);
+    const res = await axios.get(url);
+    getPokemon(res.data.results);
+    setLoading(false);
+  };
+  const getPokemon = async (res) => {
+    res.map(async (item) => {
+      const result = await axios.get(item.url);
 
-    const pokeFun=async()=>{
-        setLoading(true)
-        const res=await axios.get(url);
+      setPokeData((state) => {
+        state = [...state, result.data];
+        state.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return state;
+      });
+    });
+  };
 
-  
-        getPokemon(res.data.results);
-        setLoading(false)
-    }
-    const getPokemon=async(res)=>{
-       res.map(async(item)=>{
-          const result=await axios.get(item.url)
-    
-          
-          setPokeData(state=>{
-              state=[...state,result.data]
-              state.sort((a,b)=>a.id>b.id?1:-1)
-              return state;
-          })
-       })   
-    }
-
-
-
-
-     
-    useEffect(()=>{
-        pokeFun();
-    },[url])
-    return(
-        <>
-        <div className="search">
-
-                <Search search={pokeData} />
+  useEffect(() => {
+    pokeFun();
+  }, [url]);
+  return (
+    <>
+      <div className="search">
+        <Search search={pokeData} />
+      </div>
+      <div className="container">
+        <div className="left-content">
+          <Card
+            pokemon={pokeData}
+            loading={loading}
+            infoPokemon={(poke) => setPokeDex(poke)}
+          />
         </div>
-            <div className="container">
-                <div className="left-content">
-                    <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>
-                    
-                    
-                </div>
-                <div className="right-content">
-                   <Pokeinfo data={pokeDex}/>
-                </div>
-            </div>
-        </>
-    )
-}
+        <div className="right-content">
+          <Pokeinfo data={pokeDex} />
+        </div>
+      </div>
+    </>
+  );
+};
 export default Main;
